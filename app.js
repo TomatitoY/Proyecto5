5ba354a3c1d27dae0e7e56752b84dbba2802d8af
Yaretzi <yaretzi11mtz@gmail.com>
2025-07-24 13:28:18 -0600

    actividad de clase 24/07

---
+++ app.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"; //conectar una base de datos
+import cors from "cors"; // dar seguridad a un server
+import dotenv from "dotenv"; // poder llamar lo que hay en .env
+import express from  "express"; // para crear un servidr local
+
+//Importaciones de librerias
+
+dotenv.config();
+mongoose.connect(process.env.urlBD) //conectar con mongodb compass
+.then(()=>{
+    console.log("Funciona chingon la base de datos 7u7")
+})
+.catch((error)=>{
+    console.log("No jalo la chingadera", error)
+})   //promesa de verificacion de funcionamiento
+
+// .then sino encuentra nada funciono
+
+const app = express(); // servidor
+app.use(cors());
+app.listen(4000, ()=>{
+    console.log("Se escucha el servidor, no se escucha borroso")
+}) // verificar si funciona e servidor 
